52e58c9bd40723f92778239d90db8e4be1cb875d
Ilshat Shamilov <ilshat200127@mail.ru>
2022-11-26 16:56:36 +0300

    second

--- main.py
+++ main.py
@@ -1,42 +1,38 @@
-from PyQt5.QtWidgets import QWidget, QApplication, QMainWindow, QLabel, QGridLayout
-from PyQt5.QtGui import QPainter, QPixmap, QPen, QColor
-from PyQt5.QtCore import Qt
+import sys
+
+import random
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtGui import QPainter, QColor
 from PyQt5 import uic
-from random import randint
 
 
-class Test(QMainWindow):
+class yellow_circles(QMainWindow):
     def __init__(self):
         super().__init__()
         uic.loadUi('UI.ui', self)
+        self.qp = QPainter()
+        self.flag = False
+        self.add.clicked.connect(self.drawf)
 
-        self.pushButton.setFixedSize(100, 100)
-        self.pushButton.clicked.connect(self.circle)
-
-        self.label = QLabel()
-        canvas = QPixmap(600, 600)
-        self.label.setPixmap(canvas)
-
-        layout = QGridLayout(self.centralwidget)
-        layout.addWidget(self.pushButton, 0, 0, alignment=Qt.AlignCenter)
-        layout.addWidget(self.label, 1, 0)
-
-    def circle(self):
-        x, y = [randint(10, 500) for i in range(2)]
-        w, h = [randint(10, 100) for i in range(2)]
-        painter = QPainter(self.label.pixmap())
-        pen = QPen()
-        pen.setWidth(3)
-        pen.setColor(QColor(*[randint(0, 255) for i in range(3)]))
-        painter.setPen(pen)
-        painter.drawEllipse(x, y, w, h)
-        painter.end()
+    def drawf(self):
+        self.flag = True
         self.update()
+
+    def paintEvent(self, event):
+        if self.flag:
+            self.qp = QPainter()
+            self.qp.begin(self)
+            self.draw()
+            self.qp.end()
+
+    def draw(self):
+        self.qp.setBrush(QColor(255, 255, 0))
+        x = random.randint(1, 75)
+        self.qp.drawEllipse(random.randint(0, 1404), random.randint(0, 927), x, x)
 
 
 if __name__ == '__main__':
-    import sys
     app = QApplication(sys.argv)
-    w = Test()
-    w.show()
+    ex = yellow_circles()
+    ex.show()
     sys.exit(app.exec_())
